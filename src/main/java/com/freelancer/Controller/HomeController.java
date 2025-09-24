91a22720a796125a490ec47432f63df5e0399653
nrevanthkumarreddy <revanth12428@gmail.com>
2025-09-24 21:42:34 +0530

    first commit

---
+++ src/main/java/com/freelancer/Controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.freelancer.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Backend up and running";
+    }
+
+}
